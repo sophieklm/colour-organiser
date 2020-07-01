8503e39d6fc9f3ffa4816164044305bbf3d6a4c4
Sophie Mann <sklm@live.co.uk>
2020-07-01 13:17:38 +0100

    Add create color functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import colorData from "./color-data.json";
 import ColorList from "./ColorList.js";
 import AddColorForm from "./AddColorForm";
+import { v4 } from "uuid";
 
 export default function App() {
   const [colors, setColors] = useState(colorData);
@@ -18,11 +19,22 @@ export default function App() {
     setColors(newColors);
   };
 
+  const createColor = (title, color) => {
+    const newColors = [
+      ...colors,
+      {
+        id: v4(),
+        rating: 0,
+        title,
+        color,
+      },
+    ];
+    setColors(newColors);
+  };
+
   return (
     <>
-      <AddColorForm
-        onNewColor={(title, color) => alert(`TODO: Create ${title} - ${color}`)}
-      />
+      <AddColorForm onNewColor={createColor} />
       <ColorList
         colors={colors}
         onRemoveColor={removeColor}
